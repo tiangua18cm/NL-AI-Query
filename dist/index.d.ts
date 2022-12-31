280377cd7ec3857ab40d73b0665ea72f93c3663e
tiangua18cm <tiangua18cm@proton.me>
2022-12-31 06:39:19 +0000

    100% coverage

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,4 +4,4 @@ import { MongoClient } from "mongodb";
  * AIQuery is a class that simplifies the process of interacting with a MongoDB database
  * by allowing users to make advanced queries using natural language text. It utilizes
  * the OpenAI GPT-4 model to generate executable TypeScript code based on the given query,
- * roles, schemas, an
+ * roles, schemas, and database type, allowing for flexible and dynamic dat
