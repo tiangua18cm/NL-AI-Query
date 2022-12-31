a5d84115ddd3a0729218d7da352118abb6184226
tiangua18cm <tiangua18cm@proton.me>
2022-12-31 05:58:29 +0000

    bootstrap added

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,4 +3,5 @@ import { MongoClient } from "mongodb";
  * ANCHOR: AIQuery
  * AIQuery is a class that simplifies the process of interacting with a MongoDB database
  * by allowing users to make advanced queries using natural language text. It utilizes
- * the OpenAI GPT-4 model to generate executable TypeSc
+ * the OpenAI GPT-4 model to generate executable TypeScript code based on the given query,
+ * roles, schemas, an
