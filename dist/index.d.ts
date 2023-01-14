e721510645f425b1b3078049ee8ad6bb77e44e56
tiangua18cm <tiangua18cm@proton.me>
2023-01-14 07:27:32 +0000

    Date fix

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -14,4 +14,5 @@ import { MongoClient } from "mongodb";
  * The process involves creating a detailed and specific prompt for GPT-4 based on the
  * input query, schemas, roles, database type, and additional logic. The AI model then generates
  * a TypeScript code snippet that performs the desired database operation using the provided
- * schemas and connection. The generated c
+ * schemas and connection. The generated code is validated and executed using either eval or vm2,
+ * and 
