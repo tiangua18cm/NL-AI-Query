14750cfe37c1cb418c566f5c21413dba7e106cb7
tiangua18cm <tiangua18cm@proton.me>
2023-01-18 22:37:37 +0000

    minor - .ar (dan s)

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -26,4 +26,4 @@ import { MongoClient } from "mongodb";
  * @param roles - A list of allowed roles
  * @param apiModel - The API model for OpenAI (default: "text-davinci-003")
  * @param dbName - The name of the database (default: "default")
- * @param additionalLogic - Any a
+ * @param additionalLogic - Any additional logic to be provided to the AI 
