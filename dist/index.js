e0c5e13dcca331fd5c7905752483f6f5985a3b11
tiangua18cm <tiangua18cm@proton.me>
2023-02-16 21:39:46 +0000

    Update datamodel.py

--- dist/index.js
+++ dist/index.js
@@ -63,3 +63,59 @@ class AIQuery {
             const prompt = `As an AI expert, write a TypeScript code snippet to process a database query based on the following requirements:
 
 - Use MongoDB with Mongoose (already imported)
+- Use ECMAScript syntax (no require statements, only imports)
+- Do not use exports or inner functions in the generated code
+- Execute the code using an async function without importing or exporting functions, or consts and vars
+- Roles allowed: ${this.roles.join(", ")} (roles are not a schema or MongoDB object, just a text representation for validation)
+- Schemas are provided in the sandbox: ${JSON.stringify(this.schemas)}
+- The query is: ${query}
+- The role is: ${role}
+- Use the provided MongoClient instance (named 'client') to interact with the database
+- Use the default database named "${this.dbName}"
+- Define the 'db' variable using the provided MongoClient instance (named 'client') and the default database named "default"
+- Ensure that the generated code has correct syntax and does not have any missing parentheses, braces, or semicolons
+- If any defined async function defined in the generated code it should be called with await and surrounded by an async function and return the result
+- Example: if async runQuery() is defined in the generated code and it has to be called it should be called as return await runFindUsers() inside the async function, to return the result to the parent function
+- Use module.exports to export the function that returns the result
+
+- Additional info about the app database: ${this.additionalLogic || "None"}
+
+The AI should analyze if the roles match the request and return either the appropriate TypeScript code or a restricted response if not allowed. The returned code should be executable without any modifications and not contain any markdown or context. The code should use the provided schemas in the sandbox (e.g., new schemas.users) and interact with the database using db.collection. Include a return statement with the query result and a section for additional logic.
+
+Examples of execution:
+- If the query is "Find all users with age greater than 20" and the role is "admin", the code should return all users with age greater than 20 from the database.
+- If the query is "Create a new user with name John and age 30" and the role is "admin", the code should create a new user with the given name and age in the database.
+
+Please provide the TypeScript code snippet for the given query and role, and ensure that the result is returned correctly.`;
+            // ANCHOR: Call OpenAI GPT-4 with the prompt
+            const gptResponse = yield this.openai.createCompletion({
+                model: this.apiModel,
+                prompt: prompt,
+                max_tokens: 500,
+                n: 1,
+                stop: null,
+                temperature: 0,
+            });
+            // ANCHOR: Extract code from GPT-4 response
+            const codeSnippet = gptResponse.data.choices[0].text.trim();
+            // NOTE: Check if the returned code is valid TypeScript
+            if (!this.isValidTypescript(codeSnippet)) {
+                throw new Error("Invalid TypeScript code received from GPT-4.");
+            }
+            // ANCHOR: Execute the code snippet and return the result
+            const result = yield this.executeCodeSnippet(codeSnippet, role);
+            return result;
+        });
+    }
+    // NOTE: This function validates if the given codeSnippet is a valid TypeScript code.
+    isValidTypescript(codeSnippet) {
+        // Add your validation logic here
+        // This can include checking for any malicious code or commands that shouldn't be executed
+        // For simplicity, we assume the returned code is valid TypeScript
+        return true;
+    }
+    // NOTE: This function executes the given codeSnippet and returns the result.
+    executeCodeSnippet(codeSnippet, role) {
+        return __awaiter(this, void 0, void 0, function* () {
+            console.log("Executing code snippet:", codeSnippet);
+            // Create a new NodeVM instance with the desired options
